225045b53ba54683a546f4be3969591e99c7b866
Monkey911 <andreas.baum95@hotmail.com>
2017-12-22 13:08:21 +0200

    Fixed one problematic place in imdb_data #1

--- imdb_data_scripts/imdb_dataEdit.R
+++ imdb_data_scripts/imdb_dataEdit.R
@@ -4,12 +4,9 @@ imdb3 <- read.csv("c:/kool/andmekaeve/movies_project/Data/imdb/imdb_data3.csv")
 
 library(dplyr)
 
-imdb1$critics_count[is.na(imdb1$critics_count)] = 0
-imdb1$review_count[is.na(imdb1$review_count)] = 0
-
-
 data <- rbind(imdb1, imdb2, imdb3)
 important_data <- data %>% filter(!is.na(rating))
+important_data <- important_data %>% filter(imdb_id != 0)
 
 important_data$critics_count[is.na(important_data$critics_count)] = 0
 important_data$review_count[is.na(important_data$review_count)] = 0
@@ -17,3 +14,4 @@ important_data$review_count[is.na(important_data$review_count)] = 0
 important_data <- important_data %>% select(-X)
 
 write.csv(important_data, file="c:/kool/andmekaeve/movies_project/Data/imdb/imdb_data.csv")
+

4ea10cc250c07589b4ebe784eb2c37230c4b3d98
Monkey911 <andreas.baum95@hotmail.com>
2017-12-23 17:32:15 +0200

    imdb data cleaned

--- imdb_data_scripts/imdb_dataEdit.R
+++ imdb_data_scripts/imdb_dataEdit.R
@@ -12,6 +12,46 @@ important_data$critics_count[is.na(important_data$critics_count)] = 0
 important_data$review_count[is.na(important_data$review_count)] = 0
 
 important_data <- important_data %>% select(-X)
+important_data <- important_data[!duplicated(important_data),]
+
+temp <-  important_data %>% filter(imdb_id == "tt0127834")
+temp <- temp %>% arrange(rating_count)
+row_to_add <- temp[1,]
+important_data <- important_data %>% filter(imdb_id != "tt0127834")
+important_data <- rbind(important_data, row_to_add)
+
+temp <-  important_data %>% filter(imdb_id == "tt0235679")
+temp <- temp %>% arrange(rating_count)
+row_to_add <- temp[1,]
+important_data <- important_data %>% filter(imdb_id != "tt0235679")
+important_data <- rbind(important_data, row_to_add)
+
+temp <-  important_data %>% filter(imdb_id == "tt0287635")
+temp <- temp %>% arrange(rating_count)
+row_to_add <- temp[1,]
+important_data <- important_data %>% filter(imdb_id != "tt0287635")
+important_data <- rbind(important_data, row_to_add)
+  
+temp <-  important_data %>% filter(imdb_id == "tt0499537")
+temp <- temp %>% arrange(rating_count)
+row_to_add <- temp[1,]
+important_data <- important_data %>% filter(imdb_id != "tt0499537")
+important_data <- rbind(important_data, row_to_add)
+
+temp <-  important_data %>% filter(imdb_id == "tt0067306")
+temp <- temp %>% arrange(rating_count)
+row_to_add <- temp[1,]
+important_data <- important_data %>% filter(imdb_id != "tt0067306")
+important_data <- rbind(important_data, row_to_add)
+
+temp <-  important_data %>% filter(imdb_id == "tt2121382")
+temp <- temp %>% arrange(rating_count)
+row_to_add <- temp[1,]
+important_data <- important_data %>% filter(imdb_id != "tt2121382")
+important_data <- rbind(important_data, row_to_add)
+
+head(sort(table(important_data$imdb_id),decreasing=T), 15)
+
 
 write.csv(important_data, file="c:/kool/andmekaeve/movies_project/Data/imdb/imdb_data.csv")
 

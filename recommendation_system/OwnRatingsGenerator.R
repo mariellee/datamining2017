f67d885fa573ef35fc236556f15c2e0ea419ec78
Monkey911 <andreas.baum95@hotmail.com>
2018-01-04 14:05:59 +0200

    My own ratings which I will use for getting the recommendations later #15

---
+++ recommendation_system/OwnRatingsGenerator.R
@@ -0,0 +1,13 @@
+metascores <- read.csv("C:/kool/andmekaeve/movies_project/recommendation_system/samples/metascores.csv")
+metadata <- read.csv("C:/kool/andmekaeve/project/the-movies-dataset/movies_metadata.csv")
+metadata <- metadata %>% select (id, title)
+
+input <- read.csv("C:/kool/andmekaeve/movies_project/Data/movie set kaggle/sample_input.csv")
+
+movie_ids <- c(155, 13, 11, 12347, 211672, 16538, 131631)
+ratings <- c(10, 6, 4, 8, 9, 7, 10)
+
+my_id <- 1111110
+
+output <- data.frame(userId = rep(my_id, length(ratings)), movieId = movie_ids, rating = ratings)
+write.csv(output, file="C:/kool/andmekaeve/movies_project/recommendation_system/samples/input.csv")

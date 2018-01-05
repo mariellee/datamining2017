346c45fcfeb22f23341a48d1fa2b21e1f8f913db
Monkey911 <andreas.baum95@hotmail.com>
2018-01-05 03:35:24 +0200

    Still tried to do similar clients #7

--- recommendation_system/other_similar_people.R
+++ recommendation_system/other_similar_people.R
@@ -10,12 +10,15 @@ meta_data <- read.csv("C:/kool/andmekaeve/project/the-movies-dataset/movies_meta
 meta_data <- meta_data %>% select(id, title)
 meta_data$predicton_score <- rep(0, nrow(meta_data))
 
-
 suitable <- ratings %>% filter (movieId %in% input$movieId)
 
 users <- unique(suitable$userId)
+print(leng(users))
+counter <-  1
 
 for (user in users){
+  print(counter)
+  counter <- counter + 1
   user_ratings <- ratings %>% filter (userId == user)
   closeness <- 0
   for (rating_row in c(1:nrow(user_ratings))) {
@@ -36,12 +39,14 @@ for (user in users){
     }
   }
   if (closeness > 0) {
-      for (rating_row in c(1:nrow(user_ratings))) {
-        m_id <- user_ratings[rating_row, 2]
-        m_rating <- user_ratings[rating_row, 3]
-        score <- closeness * m_rating / 10
-        row_nr <- which(meta_data[,1] == m_id)
-        meta_data[row_nr, 3] <- meta_data[row_nr, 3] + score
+      maximum <- max(user_ratings$rating, na.rm = TRUE)
+      liked <-  user_ratings %>% filter (rating == maximum)
+      for (i in c(1:nrow(liked))) {
+        m_id <- liked[i,2]
+        m_rating <- liked[i,3]
+        r_row <- which(meta_data[,1] == m_id)
+        score <- closeness*maximum /10
+        meta_data[r_row, 3] <- score + meta_data[r_row, 3]
       }
   }
 }
